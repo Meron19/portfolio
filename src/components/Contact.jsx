279ae0e616cf4b0170dee1151fac02025531e19d
Meron19 <meronamershaoo@gmail.com>
2024-07-09 15:38:24 -0700

    Configure React app for deployment to GitHub Pages

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,52 +1,78 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import emailjs from '@emailjs/browser';
+import axios from 'axios';
+
 
 export const Contact = () => {
-    const form = useRef();
 
-    const sendEmail = (e) => {
-        e.preventDefault();
-    
-        emailjs
-          .sendForm('service_xqxi455', 'template_wclqxei', form.current, {
-            publicKey: 'weCGYQRWuAkXjvy4z',
-          })
-          .then(
-            () => {
-              console.log('Thank you for contacting me! I will get back to you as soon as possible.');
-            },
-            (error) => {
-              console.log('FAILED...', error.text);
-            },
-          );
-      };
-    
-    return (
-        <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-            {/* <form  ref={form} onSubmit={sendEmail} className='flex flex-col max-w-[600px] w-full'>
-                <div className='pb-8'>
-                    <p className='text-4xl font-bold inline border-b-4 border-red-600 text-gray-300'>Contact</p>
-                    <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - meronamershaoo@gmail.com</p>
-                </div>
-                <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
-                <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
-                <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
-                <button className='text-white border-2 hover:bg-red-600 hover:border-red-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
-            </form> */}
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
-            <form ref={form} onSubmit={sendEmail} className='flex flex-col max-w-[600px] w-full'>
-                <div className='pb-8'>
-                    <p className='text-4xl font-bold inline border-b-4 border-red-600 text-gray-300'>Contact</p>
-                    <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - meronamershaoo@gmail.com</p>
-                </div>                
-                {/* <label>Name</label> */}
-                <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name="user_name" />
-                {/* <label>Email</label> */}
-                <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name="user_email" />
-                {/* <label>Message</label> */}
-                <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message' />
-                <input className='text-white border-2 hover:bg-red-600 hover:border-red-600 px-4 py-3 my-8 mx-auto flex items-center' type="submit" value="Let's Collaborate" />
-            </form>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Your EmailJS service ID, template ID, and Public Key
+    const serviceId = 'service_xqxi455';
+    const templateId = 'template_wclqxei';
+    const publicKey = 'weCGYQRWuAkXjvy4z';
+
+    // Create an object with EmailJS service ID, template ID, Public Key, and Template params
+    const data = {
+      service_id: serviceId,
+      template_id: templateId,
+      user_id: publicKey,
+      template_params: {
+        user_name: name,
+        user_email: email,
+        to_name: 'Meron Amer Shaoo',
+        message: message,
+      }
+    };
+
+    // Send the email using EmailJS
+    try {
+      const res = await axios.post("https://api.emailjs.com/api/v1.0/email/send", data);
+      console.log(res.data);
+      setName('');
+      setEmail('');
+      setMessage('');
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col max-w-[600px] w-full'>
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline border-b-4 border-red-600 text-gray-300'>Contact</p>
+          <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - meronamershaoo@gmail.com</p>
         </div>
-      )
+        <input
+          className='bg-[#ccd6f6] p-2'
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          className='my-4 p-2 bg-[#ccd6f6]'
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <textarea
+          className='bg-[#ccd6f6] p-2'
+          cols="30"
+          rows="10"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        >
+        </textarea>
+        <button className='text-white border-2 hover:bg-red-600 hover:border-red-600 px-4 py-3 my-8 mx-auto flex items-center' type="submit">Let's Collaborate</button>
+      </form>
+    </div>
+  )
 }
